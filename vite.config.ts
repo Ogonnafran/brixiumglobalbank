01c7330a5f73252b75bc8f2728719e32538d857f
francis <francisogonna423@gmail.com>
2025-06-19 00:28:33 +0100

    prepare for github pages deploy 2nd testing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/brixiumglobalbank/', // Replace <repo-name> with your GitHub repository name
+  base: '/', // Replace <repo-name> with your GitHub repository name
   server: {
     port: 3000
   }
